064df05c9744c60cf76042f0e8f28b93864ccb9b
rob <robertmaticke12@gmail.com>
2022-08-19 23:59:37 -0400

    commit tree eed101916af4bcba125f4dbf5e9e2778cfdcf908

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName){
+    let message = []
+    for (let i = 0; i < namesArray.length; i++) {
+        message.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+
+    return message
+}
+
+
+function countDown(n){
+    while (n >= 0 ) {
+        console.log(n--);
+    }
+}
